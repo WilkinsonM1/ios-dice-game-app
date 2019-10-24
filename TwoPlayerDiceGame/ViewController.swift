c77e96b45a4d93f2a0a4f46a0830d29f4e5b7581
CN72 <cn72@CN72.local>
2019-10-24 10:17:29 +0100

    Initial Commit

---
+++ TwoPlayerDiceGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwoPlayerDiceGame
+//
+//  Created by CN72 on 24/10/2019.
+//  Copyright © 2019 CN72. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
